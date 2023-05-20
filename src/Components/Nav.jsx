c9f44ffb9c14e46581db375e7c8f849710707a8c
Pkanand1999 <singhisking810267@gmail.com>
2023-05-20 17:33:53 +0530

    assignment

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+  } from '@chakra-ui/react'
+
+export default function Nav() {
+  return (
+    <>
+  </>
+  )
+}
